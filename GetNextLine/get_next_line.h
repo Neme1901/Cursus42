ca09c74232fb72ec2dcec6cb8cb9a28001408ae2
Daniel Barrios Vázquez <dani@MacBook-Air-de-Daniel.local>
2024-10-21 12:11:42 +0200

    1

--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -1,18 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbarrios <dbarrios@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/19 14:02:01 by dbarrios          #+#    #+#             */
-/*   Updated: 2024/10/21 12:04:16 by dbarrios         ###   ########.fr       */
+/*   Created: 2024/10/21 11:17:38 by dbarrios          #+#    #+#             */
+/*   Updated: 2024/10/21 11:22:02 by dbarrios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include 'get_next_line.h'
-
-char	get_next_line(int fd)
-{}
-
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#endif GET_NEXT_LINE_H
