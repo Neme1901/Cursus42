5ea3617876d83e3883dee5ab1277d5fa28aee4f5
Daniel Barrios Vazquez <dbarrios@c3r13s2.42madrid.com>
2024-10-23 21:06:32 +0200

    1

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dbarrios <dbarrios@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:02:01 by dbarrios          #+#    #+#             */
-/*   Updated: 2024/10/21 12:21:53 by dbarrios         ###   ########.fr       */
+/*   Updated: 2024/10/23 21:03:18 by dbarrios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,9 @@
 
 char	get_next_line(int fd)
 {
+	static char	*prev_buffer;
+	char		*line;
+	char		*buffer;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,10 +6,17 @@
 /*   By: dbarrios <dbarrios@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:17:38 by dbarrios          #+#    #+#             */
-/*   Updated: 2024/10/21 11:22:02 by dbarrios         ###   ########.fr       */
+/*   Updated: 2024/10/23 20:53:50 by dbarrios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+
 #endif GET_NEXT_LINE_H
