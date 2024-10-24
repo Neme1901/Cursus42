bff804062f6fed230942d06021520a385ca914b8
Daniel Barrios Vazquez <dbarrios@c1r2s6.42madrid.com>
2024-10-24 07:04:12 +0200

    1

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,19 +6,15 @@
 /*   By: dbarrios <dbarrios@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:02:01 by dbarrios          #+#    #+#             */
-/*   Updated: 2024/10/23 21:14:29 by dbarrios         ###   ########.fr       */
+/*   Updated: 2024/10/24 07:01:21 by dbarrios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*prev_buffer;
 	char		*line;
 	char		*buffer;
-
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (fd < 0 || !buffer)
-		return (free(buffer, prev_buffer), NULL);
 }
