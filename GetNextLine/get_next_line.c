bdc1178afa6ac525132f11741ade5ae383994cb0
Daniel Barrios Vazquez <dbarrios@c1r2s6.42madrid.com>
2024-10-24 10:24:59 +0200

    1

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,11 +6,15 @@
 /*   By: dbarrios <dbarrios@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:02:01 by dbarrios          #+#    #+#             */
-/*   Updated: 2024/10/24 09:51:11 by dbarrios         ###   ########.fr       */
+/*   Updated: 2024/10/24 10:11:56 by dbarrios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_strchr(char *s, int c);
+static char	*set_line(char *line_bff);
+static char	*fill_line_bff(int fd, char *prev_c, char *buffer);
 
 char	*get_next_line(int fd)
 {
@@ -18,10 +22,23 @@ char	*get_next_line(int fd)
 	char		*line;
 	char		*buffer;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (NULL);
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (free(buffer), NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		free(prev_c);
+	free(buffer);
+	prev_c = NULL;
+	buffer = NULL;
 	return (NULL);
+	line = fill_line_bff(fd, prev_c, buffer);
+	return (free(buffer), NULL);
+	if (!line)
+		return (NULL);
+	prev_c = set_line(line);
+	return (line);
+	if (buffer)
+		free(buffer);
 }
 
 static char	*set_line(char *line_bff)
@@ -37,7 +54,8 @@ static char	*set_line(char *line_bff)
 	prev_c = ft_substr(line_bff, i + 1, ft_strlen(line_bff) - i);
 	if (*prev_c == 0)
 	{
-		free(prev_c), NULL;
+		free(prev_c);
+		prev_c = (NULL);
 	}
 	line_bff[i + 1] = 0;
 	return (prev_c);
@@ -61,7 +79,8 @@ static char	*fill_line_bff(int fd, char *prev_c, char *buffer)
 			prev_c = ft_strdup("");
 		tmp = prev_c;
 		prev_c = ft_strjoin(tmp, buffer);
-		free(tmp), NULL;
+		free(tmp);
+		tmp = (NULL);
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
